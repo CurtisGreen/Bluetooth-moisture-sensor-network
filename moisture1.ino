b9ca84363125357a9db0e5be488f5610a5becfcd
jingyu-lu <jingyu_lu@tamu.edu>
2017-11-12 14:58:54 -0600

    Add files via upload
    moisture reading

---
+++ moisture1.ino
@@ -0,0 +1,32 @@
+#include "LowPower.h"
+#include <SoftwareSerial.h>
+
+int sensor_pin = A0; 
+int output_value;
+int inByte = 0;
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Reading From the Sensor ...");
+  delay(2000);
+  }
+
+void loop() {
+  if (Serial.available()){  
+    for(int i = 0; i<15; i++)
+    {
+    output_value= analogRead(sensor_pin);
+    Serial.print("Mositure : ");
+    Serial.println(output_value);
+    Serial.write(output_value);
+    delay(1000);
+     for(int j = 0; j<5400; j++)
+     {
+      LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+     }
+    }
+  }
+}
+
+
